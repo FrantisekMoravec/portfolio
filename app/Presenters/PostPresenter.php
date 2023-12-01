b341d411416b0469fa7d85acb6ffaa4d0f33496a
František Moravec <fmoravec45@gmail.com>
2023-12-01 15:13:25 +0100

    Pohyb mezi příspěvky pro uživatele.

--- app/Presenters/PostPresenter.php
+++ app/Presenters/PostPresenter.php
@@ -5,23 +5,41 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
-use Nette\Application\UI\Form;
-use Nette\Utils\Image;
+use App\Model\PostFacade;
 
 final class PostPresenter extends Nette\Application\UI\Presenter
 {
     private Nette\Database\Explorer $database;
+    private PostFacade $facade;
 
-    public function __construct(Nette\Database\Explorer $database)
+    public function __construct(Nette\Database\Explorer $database, PostFacade $facade)
     {
         $this->database = $database;
+        $this->facade = $facade;
     }
 
     public function renderShow(int $postId): void
     {
-        $post = $this->database->table('posts')->get($postId);
-        if (!$post){
-            $this->error('Příspěvek nenalezen');
+        $post = $this->database
+            ->table('posts')
+            ->get($postId);
+
+        $postCount = $this->facade
+            ->getPostCount();
+
+        if (!$post) {
+            if ($postId < 1){
+                $postId = $postCount;
+            }
+            if ($postId > $postCount){
+                $postId = 1;
+            }
+            $post = $this->database
+                ->table('posts')
+                ->get($postId);
+            if (!$post){
+                $this->error('Příspěvek nebyl nelezen');
+            }
         }
 
         $this->template->post = $post;
